458084e8d0eef06c3ea99399c0a98c3ec3532074
YousefElsebaiy <145772263+YousefElsebaiy@users.noreply.github.com>
2024-03-14 14:28:00 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -9,49 +9,49 @@ let first = document.getElementById("first")
 let center = document.getElementById("center")
 let last = document.getElementById("last")
 let links = document.querySelector(".links")
-home.addEventListener('click', function(){
+home.addEventListener('click', function () {
     window.scroll({
         top: 0,
         left: 0,
         behavior: 'smooth',
     })
 })
-about.addEventListener('click', function(){
+about.addEventListener('click', function () {
     window.scroll({
         top: 660,
         left: 0,
         behavior: 'smooth',
     })
 })
-services.addEventListener('click', function(){
+services.addEventListener('click', function () {
     window.scroll({
         top: 870,
         left: 0,
         behavior: 'smooth',
     })
 })
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     window.scroll({
         top: 660,
         left: 0,
         behavior: 'smooth',
     })
 })
-project.addEventListener('click', function(){
+project.addEventListener('click', function () {
     window.scroll({
         top: 1300,
         left: 0,
         behavior: 'smooth',
     })
 })
-contact_us.addEventListener('click', function(){
+contact_us.addEventListener('click', function () {
     window.scroll({
         top: 2000,
         left: 0,
         behavior: 'smooth',
     })
 })
-menu.addEventListener('click', function(){
+menu.addEventListener('click', function () {
     first.classList.toggle("first")
     center.classList.toggle("center")
     last.classList.toggle("last")
